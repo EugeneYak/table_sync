881a0dc94a0f56ae9b0a13d3b4f441d3331db45b
yak <yak@zivitech.com>
2019-07-01 16:42:18 +0300

    refactoring receive notifications

--- lib/table_sync/receiving_handler.rb
+++ lib/table_sync/receiving_handler.rb
@@ -8,14 +8,6 @@ class TableSync::ReceivingHandler < Rabbit::EventHandler
   attribute :version
 
   def call
-    ActiveSupport::Notifications.instrument instrument_name, instrument_payload do
-      run
-    end
-  end
-
-  private
-
-  def run
     raise TableSync::UndefinedConfig.new(model) if configs.blank?
 
     configs.each do |config|
@@ -36,6 +28,8 @@ class TableSync::ReceivingHandler < Rabbit::EventHandler
       end
     end
   end
+
+  private
 
   def data=(data)
     @data = data[:attributes]
@@ -78,16 +72,5 @@ class TableSync::ReceivingHandler < Rabbit::EventHandler
         row unless config.skip(original_row_for_data)
       end.compact.presence
     end.compact
-  end
-
-  def instrument_name
-    "tablesync.receive.#{event}"
-  end
-
-  def instrument_payload
-    {
-      event: event,
-      model: model,
-    }
   end
 end
